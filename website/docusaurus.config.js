bf60e8760a84b325dcf652ce18851ec98556bedc
codewithtayyabashahid <biya4409@gmail.com>
2025-12-10 21:55:31 +0500

    first commit

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Tayyaba Shahid. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Laiba Shahid. Built with Docusaurus.`,
       },
       colorMode: {
         defaultMode: 'dark',
